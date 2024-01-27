e41008f24991ad6fd20d1539fb9aa64aca706c13
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2024-01-27 16:52:04 +0200

    Update Bayes' Theorem.m

--- Statistics/Bayes' Theorem.m
+++ Statistics/Bayes' Theorem.m
@@ -4,4 +4,8 @@ while true
     % Get user input for event names
     event_A_name = input('Enter the name of event A: ', 's');
     event_B_name = input('Enter the name of event B: ', 's');
+
+    % Get user input for prior probabilities
+    prior_prob_A = input(['Enter the prior probability of ' event_A_name ' (between 0 and 1): ']);
+    prior_prob_B = input(['Enter the prior probability of ' event_B_name ' (between 0 and 1): ']);
 end
