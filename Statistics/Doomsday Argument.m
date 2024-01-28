cf34476a90bd23349756e8a3e3f64598f3b1d4e9
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2024-01-28 19:17:34 +0200

    Update Doomsday Argument.m

--- Statistics/Doomsday Argument.m
+++ Statistics/Doomsday Argument.m
@@ -1,3 +1,8 @@
 function doomsdayArgument() 
+    % Display information about the Doomsday Argument
+    disp('Welcome to the Doomsday Argument Estimator!');
+    disp('The Doomsday Argument suggests that your birth rank among all humans who will ever live is likely to be around the median.');
+    disp('Enter your birth rank and estimate the total number of humans who will ever live to calculate the probability of the world ending soon.');
+
     
 end
