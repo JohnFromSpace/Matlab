b354d008b7dc57b1901dde7e9be227d3b54470c0
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2024-01-29 18:27:38 +0200

    Update Sunrise Problem.m

--- Statistics/Sunrise Problem.m
+++ Statistics/Sunrise Problem.m
@@ -1,1 +1,6 @@
+function solarInfo = calculateSolarInfo(latitude, longitude, dateStr, timeZone)
+    % Function to calculate sunrise, sunset, and moon-related information using Laplace's method and SAMPA
 
+    % Add the path to the SAMPA library
+    addpath('path_to_sampa_library');
+end    
