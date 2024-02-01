dd14a28b3c33cfc2df91fff527ea2441d72e72ca
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2024-02-01 23:13:53 +0200

    Update ODE Solver.m

--- Differential Equations/ODE Solver.m
+++ Differential Equations/ODE Solver.m
@@ -1,3 +1,6 @@
 function solve_and_plot_ode(a_values, tspan, y0, varargin)
-
+    % Check if 'a_values' is a scalar, if so, convert it to a vector
+    if isscalar(a_values)
+        a_values = [a_values];
+    end
 end
