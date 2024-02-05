a8ddcd75760d26429a0e755c02f88aa6b19c66c8
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2024-02-05 17:10:39 +0200

    Update Parabolic PDE Solver.m

--- Differential Equations/Parabolic PDE Solver.m
+++ Differential Equations/Parabolic PDE Solver.m
@@ -1,1 +1,11 @@
+function solveParabolicPDE(spatialGridSize, timeGridSize, saveSolution, pdeCoefficients, customInitialCondition, solverType, useAMR, showPlots, solverOptions, outputFilename, plotStyle, plotOptions, colorMap, plotTitle)
 
+    % Validate input parameters
+    validateInput(spatialGridSize, timeGridSize, solverType);
+
+    % Define spatial and temporal discretization
+    spatialGrid = linspace(0, 1, spatialGridSize);
+    timeGrid = linspace(0, 1, timeGridSize);
+
+    
+end
