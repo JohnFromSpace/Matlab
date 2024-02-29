77c7bdeb3cfead3756a44a7d87776fc38f0e4d61
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2024-02-29 23:54:46 +0200

    Update Square Packing.m

--- Geometry/Square Packing.m
+++ Geometry/Square Packing.m
@@ -1,3 +1,10 @@
 function [packed_squares, remaining_space] = pack_squares_genetic_lkh(container_width, container_height, square_sizes, population_size, generations, local_search_iterations, num_workers)
+    % Genetic Algorithm parameters
+    mutation_rate = 0.1;
+    crossover_rate = 0.8;
     
+    % Generate initial population
+    population = generate_population(population_size, square_sizes, container_width, container_height);
+    
+        
 end
