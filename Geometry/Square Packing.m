438c27576b897270dd461ca1569b41256c2ddefb
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2024-02-29 23:54:15 +0200

    Update Square Packing.m

--- Geometry/Square Packing.m
+++ Geometry/Square Packing.m
@@ -1,1 +1,3 @@
-
+function [packed_squares, remaining_space] = pack_squares_genetic_lkh(container_width, container_height, square_sizes, population_size, generations, local_search_iterations, num_workers)
+    
+end
