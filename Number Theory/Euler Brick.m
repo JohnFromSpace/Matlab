ad8702c2138a061e5ed4201dd7c3b5ec60e0d30b
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2024-03-11 23:56:22 +0200

    Update Euler Brick.m

--- Number Theory/Euler Brick.m
+++ Number Theory/Euler Brick.m
@@ -2,5 +2,8 @@ function euler_bricks = euler_brick()
     n = 100;
     euler_bricks = [];
     
+    % Precompute all possible values of d
+    d_values = round(sqrt((1:n).^2 + (1:n).^2 + (1:n).^2));
+    
         
 end
